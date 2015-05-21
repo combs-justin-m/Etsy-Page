af39f60ea1945f403e7a150720bb032a1e2911ed
Justin Combs <combs.justin.m@gmail.com>
2015-05-21 16:38:31 -0400

    results, searchparam and sort features added

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
-
+// Content - API appender //
 var $content = $('#content')
 var contentImg, title, user, price, fullItem;
 
 items.results.forEach( function (item){
-  contentImg =  '<img class="contentImg" src="' + item.Images[0].url_170x135 + '">';
-  title = '<p class="title">' + item.title + '</p>';
-  user = '<p class="user">' + item.Shop.shop_name + '</p>';
-  price = '<p class="price">' + '$' + item.price + ' ' + item.currency_code + '</p>';
+  contentImg =  '<img id="contentImg" src="' + item.Images[0].url_170x135 + '">';
+  title = '<p id="title">' + item.title + '</p>';
+  user = '<p id="user">' + item.Shop.shop_name + '</p>';
+  price = '<p id="price">' + '$' + item.price + ' ' + item.currency_code + '</p>';
 
   fullItem = '<li>'  + contentImg + title + user + price + '</li>';
   $content.append(fullItem);
 });
+
+// Search param and results counter //
+
+var searchTerm = items.params.keywords;
+var resultsNum = items.count;
+
+$('#resultsLine').append('"' + '<b>' + searchTerm + '</b>' + '"' + ' ' + '(' + resultsNum + ' Results)');
+

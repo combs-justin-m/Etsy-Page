46851b248fcfccd234d82d63566d09d0ade3e02a
Justin Combs <combs.justin.m@gmail.com>
2015-05-21 15:06:12 -0400

    object-fit and overflow ellipsis added

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 
 var $content = $('#content')
-var contentImg, description, user, price, fullItem;
+var contentImg, title, user, price, fullItem;
 
 items.results.forEach( function (item){
   contentImg =  '<img class="contentImg" src="' + item.Images[0].url_570xN + '">';
+  title = '<p class="title">' + item.title + '</p>';
 
-  fullItem = '<li>' +  contentImg + '</li>';
+
+  fullItem = '<li>'  + contentImg + title + '</li>';
   $content.append(fullItem);
 });

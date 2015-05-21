90d163a1fb1586f7459443c8bfe865f1cd5ec4b6
Justin Combs <combs.justin.m@gmail.com>
2015-05-21 15:50:58 -0400

    main content laid out

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ var $content = $('#content')
 var contentImg, title, user, price, fullItem;
 
 items.results.forEach( function (item){
-  contentImg =  '<img class="contentImg" src="' + item.Images[0].url_570xN + '">';
+  contentImg =  '<img class="contentImg" src="' + item.Images[0].url_170x135 + '">';
   title = '<p class="title">' + item.title + '</p>';
+  user = '<p class="user">' + item.Shop.shop_name + '</p>';
+  price = '<p class="price">' + '$' + item.price + ' ' + item.currency_code + '</p>';
 
-
-  fullItem = '<li>'  + contentImg + title + '</li>';
+  fullItem = '<li>'  + contentImg + title + user + price + '</li>';
   $content.append(fullItem);
 });

056da53320c1285feb662b7b224f4ac09dc02a43
Justin Combs <combs.justin.m@gmail.com>
2015-05-22 10:22:24 -0400

    links added to generated content

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,10 @@ var contentImg, title, user, price, fullItem;
 
 items.results.forEach( function (item){
   contentImg =  '<img id="contentImg" src="' + item.Images[0].url_170x135 + '">';
-  title = '<p id="title">' + item.title + '</p>';
-  user = '<p id="user">' + item.Shop.shop_name + '</p>';
+  titleLink = item.url;
+  title = '<a href="' + titleLink + '">' + '<p id="title">' + item.title + '</p>' + '</a>';
+  shopLink = item.Shop.url;
+  user = '<a href="' + shopLink + '">' + '<p id="user">' + item.Shop.shop_name + '</p>' + '</a>';
   price = '<p id="price">' + '$' + item.price + ' ' + item.currency_code + '</p>';
 
   fullItem = '<li>'  + contentImg + title + user + price + '</li>';
@@ -26,6 +28,4 @@ $('dt').on('click', function (){
   $(this).addClass('active');
   $('#resultsCat').html($(this).html()); // category updater
 });
-
-
 
